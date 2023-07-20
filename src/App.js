bbfa81f653d7af493b932ce110b77d191e371d82
Christian Rodrigues Batista <chris.rod_batista@outlook.com>
2023-07-20 02:51:39 -0300

    chore: removing useless code

--- src/App.js
+++ src/App.js
@@ -23,16 +23,6 @@ const Title = styled.h2`
   align-items: center;
 `;
 
-const Button = styled.button`
-    padding: 10px;
-    cursor: pointer;
-    border-radius: 5px;
-    border: none;
-    background-color: #2c73d2;
-    color: white;
-    height: 42px;
-`;
-
 function App() {
   const [events, setEvents] = useState([]);
   const [onEdit, setOnEdit] = useState(null);
