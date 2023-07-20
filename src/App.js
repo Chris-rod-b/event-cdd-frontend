4a14c7cf896550667860009265fcd3dd90904ecf
Christian Rodrigues Batista <chris.rod_batista@outlook.com>
2023-07-19 21:52:05 -0300

    feat: removing and init my project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from "./styles/global";
+import styled from "styled-components";
+import { toast, ToastContainer} from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2`
+  display: flex;
+  align-items: center;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Title>
+          <img 
+            src="https://colodedeus.com.br/wp-content/uploads/2022/08/800px-ASSINATURA-LOGO-OFICIAL-1_LOGOTIPO.png" 
+            alt="Logo CDD"
+            class="transparent shrinkToFit" 
+            width="70"  
+          />          
+          Colo de Deus - Eventos
+        </Title>
+      </Container>
+      <ToastContainer autoClose={3000} position={toast.POSITION.BOTTOM_LEFT} />
+      <GlobalStyles />
+    </>
   );
 }
 
